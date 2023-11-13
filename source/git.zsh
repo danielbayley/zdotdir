14617f7e9f88ab995794ae4d4f71f3cf8cb7ed53
Daniel Bayley <daniel.bayley@me.com>
2023-11-13 10:01:00 +0000

    Add `git`/Hub/Kraken cli aliases

---
+++ source/git.zsh
@@ -0,0 +1,12 @@
+# https://git-scm.com/docs/git#_git_commands
+alias  clone='git clone'
+alias  stage='git stage'
+alias staged='git staged'
+alias commit='git commit'
+
+# https://cli.github.com/manual
+alias gist='gh gist'
+alias pr='gh pr'
+
+# https://gitkraken.com/cli
+alias gitkraken=gk gkc=gk
