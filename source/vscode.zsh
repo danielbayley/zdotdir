42c5129288c236cebb41845db7287e8e5096fb36
Daniel Bayley <daniel.bayley@me.com>
2023-11-17 09:32:00 +0000

    Add VS Code aliases

---
+++ source/vscode.zsh
@@ -0,0 +1,6 @@
+# https://code.visualstudio.com/api/working-with-extensions/publishing-extension#vsce
+alias vcse='npx @vscode/vsce'
+alias ovsx='npx ovsx'
+
+# https://code.visualstudio.com/docs/editor/command-line#_core-cli-options
+alias edit="${=VISUAL:-$EDITOR} --goto"
