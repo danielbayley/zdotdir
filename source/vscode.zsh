edd1fba24d81ab8db95b9d26bcbbdd1f07f479ca
Daniel Bayley <daniel.bayley@me.com>
2023-11-28 15:10:31 +0000

    Add VS `code` cli alias

--- source/vscode.zsh
+++ source/vscode.zsh
@@ -1,3 +1,6 @@
+# https://code.visualstudio.com/docs/editor/command-line
+alias vsc{ode,}=code {,vs}codium=code
+
 # https://code.visualstudio.com/api/working-with-extensions/publishing-extension#vsce
 alias vcse='npx @vscode/vsce'
 alias ovsx='npx ovsx'
