633cac93c15079843381e56183499b834663b7ab
Daniel Bayley <daniel.bayley@me.com>
2024-03-27 20:30:10 +0000

    Fix terminal tab count script

--- libexec/terminal-tabs.js
+++ libexec/terminal-tabs.js
@@ -1,5 +1,5 @@
 #! /usr/bin/osascript -l JavaScript
 ObjC.import("stdlib")
 const { applicationProcesses: { Terminal }} = Application("System Events")
-const tabs = Terminal.windows[0].tabGroups.description()
+const tabs = Terminal.windows[0]?.tabGroups.description()
 $.exit(tabs.length)
