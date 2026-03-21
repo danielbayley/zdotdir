b7943f09052208da6e054b68db16ccd0b525b927
Daniel Bayley <daniel.bayley@me.com>
2026-03-21 15:41:19 +0000

    Refactor `webloc` function

--- autoload/webloc.zsh
+++ autoload/webloc.zsh
@@ -1,14 +1,16 @@
-case $1 in
-  *.webloc) plutil -extract URL raw -expect string $1 -o -;;
-
-  *) read URL <<< https://${1//(http*s:\/\/|www.)/}
-
-    if (($# < 2)) then
-      curl --location $URL --silent |
-      egrep -om 1 '<title.+?</title>' |
-      xpath -e 'title/text()' -q | read title
-    fi
-
-    plutil -create xml1 - |
-    plutil -insert URL -string $URL - -o ${title:-$2:r}.webloc
+#! /bin/zsh --no-rcs
+case ${1:-$#} in
+  *.webloc) exec plutil -extract URL raw -expect string $1 -o -;;
+  0) exit 1
 esac
+
+read URL <<< https://${1//(http*s:\/\/|www.)/}
+
+alias curl='curl --location --silent'
+
+if (($# < 2)) curl $URL |
+  egrep -om 1 '<title.+?</title>' |
+  xpath -e 'title/text()' -q | read title
+
+plutil -create xml1 - |
+plutil -insert URL -string $URL - -o ${title:-$2:r:s/-/}${${(M)2:#-}:-.webloc}
